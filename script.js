36bfff9b88c32f77a973c6266c7b05ef5b6bfbc3
Olivia <oliviapenycoblentz@gmail.com>
2017-03-03 14:01:11 +0100

    ajout script simple

--- script.js
+++ script.js
@@ -1,74 +1,106 @@
-var playerChoice = prompt("choose a Zodiac Knight between Seiya, Shiryu, Hyoga, Shun or Ikki");
+var userChoice = prompt("choose a Zodiac Knight between Seiya, Shiryu, Hyoga, Shun or Ikki");
+var playerChoice = userChoice.toLowerCase();
+var player = document.getElementsByTagName("p")[0];
+var computer = document.getElementsByTagName("p")[1];
+var attaque = document.getElementsByTagName("p")[2];
+var winner = document.getElementsByTagName("p")[3];
+var winLose = document.getElementsByTagName("p")[4];
 var computerChoice = Math.random();
-console.log(computerChoice);
 
-if(0<computerChoice<=0.20){
-  computerChoice = "Seiya";
-}
-else if (0.20<computerChoice<=0.40) {
-  computerChoice = "Shiryu";
-}
-else if (0.40<computerChoice<=0.60) {
-  computerChoice = "Hyoga";
-}
-else if (0.60<computerChoice<=0.80) {
-  computerChoice = "Shun";
-}
-else {
-  computerChoice = "Ikki";
-} console.log("Computer: " + computerChoice);
+    if(0<computerChoice<=0.20){
+      computerChoice = "seiya";
+    }
+    else if (0.20<computerChoice<=0.40) {
+      computerChoice = "shiryu";
+    }
+    else if (0.40<computerChoice<=0.60) {
+      computerChoice = "hyoga";
+    }
+    else if (0.60<computerChoice<=0.80) {
+      computerChoice = "shun";
+    }
+    else {
+      computerChoice = "ikki";
+    }
+    console.log("Computer: " + computerChoice);
 
-var compare = function(choice1,choice2){
-  if(choice1 === choice2){
-    return "Draw";
-  }
-  if (choice1 === choice2) {
-    return "Draw",
-  }
-  else if (choice1 === "Seiya") {
-    if(choice2 === "Shun" || choice2 === "Ikki"){
-      return "Pegasasu Ryūsei Ken: Seiya wins!"
-    }
-    else{
-      return "Seiya died in one kick"
-    }
-  }
 
-  else if (choice1 === "Hyoga") {
-    if(choice2 === "Seiya" || choice2 === "Shun"){
-      return "Tou Ketsu Ken: Hyoga wins!"
-    }
-    else{
-      return "Hyoga died in one kick"
-    }
-  }
 
-  else if (choice1 === "Shiryu") {
-    if(choice2 === "Hyoga" || choice2 === "Seiya"){
-      return "Rozan Shō Ryū Ha: Shiryu wins!"
-    }
-    else{
-      return "Shiryu died in one kick"
-    }
-  }
+    var compare = function(choice1,choice2){
+      if(choice1 === choice2){
+        winLose.innerHTML ="It's a Tie";
+      }
+      if (choice1 === choice2) {
+        winLose.innerHTML ="It's a Tie";
+      }
+      else if (choice1 === "seiya") {
+        if(choice2 === "shun" || choice2 === "ikki"){
+          // alert ("Pegasasu Ryūsei Ken: Seiya wins!");
+          attaque.innerHTML = "Pegasasu Ryūsei Ken";
+          winLose.innerHTML = "WINS";
+        }
+        else{
+          // alert ( "Seiya died in one kick");
+          attaque.innerHTML = "Seiya died in one kick";
+          winLose.innerHTML = "LOSES";
+        }
+      }
 
-  else if (choice1 === "Ikki") {
-    if(choice2 === "Shiryu" || choice2 === "Hyoga"){
-      return "Hōō Genma Ken: Ikki wins!"
-    }
-    else{
-      return "Ikki died in one kick"
-    }
-  }
+      else if (choice1 === "hyoga") {
+        if(choice2 === "seiya" || choice2 === "shun"){
+          // alert ( "Tou Ketsu Ken: Hyoga wins!");
+          attaque.innerHTML = "Tou Ketsu Ken";
+          winLose.innerHTML = "WINS";
+        }
+        else{
+          // alert ( "Hyoga died in one kick");
+          attaque.innerHTML = "hyoga died in one kick";
+          winLose.innerHTML = "LOSES";
+        }
+      }
 
-  else if (choice1 === "Shun") {
-    if(choice2 === "Ikki" || choice2 === "Shiryu"){
-      return "Nebyura Chēn: Shun wins!"
-    }
-    else{
-      return "Shun died in one kick"
-    }
-  }
-};
+      else if (choice1 === "shiryu") {
+        if(choice2 === "hyoga" || choice2 === "seiya"){
+          // alert ( "Rozan Shō Ryū Ha: Shiryu wins!");
+          attaque.innerHTML = "Rozan Shō Ryū Ha";
+          winLose.innerHTML = "WINS";
+        }
+        else{
+          // alert ("Shiryu died in one kick");
+          attaque.innerHTML = "shiryu died in one kick";
+          winLose.innerHTML = "LOSES";
+        }
+      }
 
-compare(userChoice, computerChoice);
+      else if (choice1 === "ikki") {
+        if(choice2 === "shiryu" || choice2 === "hyoga"){
+          // alert ( "Hōō Genma Ken: Ikki wins!");
+          attaque.innerHTML = "Hōō Genma Ken";
+          winLose.innerHTML = "WINS";
+        }
+        else{
+          // alert ( "Ikki died in one kick");
+          attaque.innerHTML = "Ikki died in one kick";
+          winLose.innerHTML = "LOSES";
+        }
+      }
+
+      else if (choice1 === "shun") {
+        if(choice2 === "ikki" || choice2 === "shiryu"){
+          // alert ( "Nebyura Chēn: Shun wins!");
+          attaque.innerHTML = "Nebyura Chēn";
+          winLose.innerHTML = "WINS";
+        }
+        else{
+          // alert ( "Shun died in one kick");
+          attaque.innerHTML = "Shun died in one kick";
+          winLose.innerHTML = "LOSES";
+        }
+      }
+      player.innerHTML = playerChoice;
+      computer.innerHTML = computerChoice;
+    };
+
+
+
+    compare(playerChoice, computerChoice);
